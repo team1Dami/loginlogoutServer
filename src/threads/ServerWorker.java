e5754ed9a370e7e85cebcf46dec704723a86b5fd
Saray <sarayarce86@gmail.com>
2020-10-25 13:39:42 +0100

    classes threads & pool

---
+++ src/threads/ServerWorker.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author saray
+ */
+public class ServerWorker extends Thread{
+    private static final Logger logger = Logger.getLogger("threads.ServerWorker");
+    
+    
+    @Override
+    public void run() {
+        
+    }
+}
