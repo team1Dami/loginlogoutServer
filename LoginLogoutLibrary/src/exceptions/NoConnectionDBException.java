c654259e362ee0a5cd743c8d83ccda0029056131
Saray <sarayarce86@gmail.com>
2020-10-27 09:56:47 +0100

    Library

---
+++ LoginLogoutLibrary/src/exceptions/NoConnectionDBException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author saray
+ */
+public class NoConnectionDBException extends Exception implements Serializable{
+    
+}
---
+++ LoginLogoutLibrary/src/exceptions/UserExistException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author saray
+ */
+public class UserExistException extends Exception implements Serializable{
+    
+}
