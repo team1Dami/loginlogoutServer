8caa31ca0b36bddbdb92e44a41a890490e584398
Saray <sarayarce86@gmail.com>
2020-10-27 08:52:46 +0100

    ConnectionPool class (falta por terminar)  && FileProperties

--- src/Pool/ConnectionPool.java
+++ src/Pool/ConnectionPool.java
@@ -5,10 +5,41 @@
  */
 package Pool;
 
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import org.apache.commons.dbcp2.BasicDataSource;
 /**
  *
  * @author saray
  */
 public class ConnectionPool {
+    
+  private static final Logger logger = Logger.getLogger("Pool.ConnectionPool");
+  
+  // private Properties properties = new Properties();
+    private final ResourceBundle ConnectionFile;
+   
+   // private static DataSource dataSource =null;
+  // private static ConnectionPool datasource;
+    private static BasicDataSource basicDataSource=null;
+
+    public ConnectionPool(){
+        
+        this.ConnectionFile = ResourceBundle.getBundle("control.ConnectionFile");
+        
+        basicDataSource= new BasicDataSource();
+        if (null==basicDataSource){
+          //  basicDataSource = new BasicDataSource();
+            basicDataSource.setDriverClassName(this.ConnectionFile.getString("Driver"));
+            basicDataSource.setUsername(this.ConnectionFile.getString("DBUser"));
+            basicDataSource.setPassword(this.ConnectionFile.getString("DBPass"));
+            basicDataSource.setUrl(this.ConnectionFile.getString("Conn"));
+         //   basicDataSource.setMaxActive(10);
+            basicDataSource.setMinIdle(0);
+            basicDataSource.setMaxIdle(10);
+        }
+   }
     
 }
